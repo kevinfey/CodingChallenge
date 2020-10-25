dedb31c17b76bf32966c2d0363336835d184660e
KEVIN_FEY <KEVINFEY@gmail.com>
2020-10-25 00:57:45 -0700

    coding challenge complete

--- frontend/sass/index.js
+++ frontend/sass/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
@@ -7,34 +8,42 @@ const winston = require('winston');
 const PORT = 3000;
 
 const logger = winston.createLogger({
-	transports: [new (winston.transports.Console)()],
+  transports: [new winston.transports.Console()],
 });
 
 const app = express();
-
+app.use(express.json());
 // Static assets.
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Logger.
-app.use(morgan(':method :url :status :response-time ms', {
-	stream: {
-		write: message => logger.info(message.trim()),
-	},
-}));
+app.use(
+  morgan(':method :url :status :response-time ms', {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  })
+);
 
 // Configure templating engine.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
 nunjucks.configure(app.get('views'), {
-	autoescape: true,
-	express: app,
+  autoescape: true,
+  express: app,
+});
+
+app.post('/', (req, res) => {
+  const username = req.body.username;
+  const comment = req.body.comment;
+  return res.send({ username, comment });
 });
 
 app.get('/', (request, response) => {
-	const options = { pageTitle: 'Homepage' };
-	return response.render('home', options);
+  const options = { pageTitle: 'Homepage' };
+  return response.render('home', options);
 });
 
 app.listen(PORT, () => {
-	logger.log({ level: 'info', message: `listening on ${PORT}` });
+  logger.log({ level: 'info', message: `listening on ${PORT}` });
 });
